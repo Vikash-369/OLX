fea182312f2f93df6403ea73ab5a1bce675ec65a
VIKASH KUMAR <vikash9267956301@gmail.com>
2025-05-22 17:02:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSubmenu(header) {
+    const submenu = header.nextElementSibling;
+    if (submenu.style.display === "none" || submenu.style.display === "") {
+        submenu.style.display = "block";
+    } else {
+        submenu.style.display = "none";
+    }
+}
